e87c5eb49fb4c222ecbc0da2f72921943da0d0c5
uiurihappy <uiurihappy@sju.ac.kr>
2022-10-01 20:59:06 +0900

    feat: paramsInit

--- middlewares/paramsInit.middleware.ts
+++ middlewares/paramsInit.middleware.ts
@@ -1,11 +1,18 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
-import { ConfigService } from '@Src/config/config.service';
-import { Request, Response, NextFunction } from 'express';
-import * as response from '../src/common/tools/response.tool';
+import { Response, Request, NextFunction } from 'express';
+import * as geoIp from 'geoip-country';
+import * as _ from 'lodash';
+import * as requestIp from 'request-ip';
 
 @Injectable()
 export class ParamsInitMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    const geo = geoIp.lookup(requestIp.getClientIp(req));
+    if (_.has(geo, 'country')) {
+      req.params.locale = geo.country;
+    } else {
+      req.params.locale = undefined;
+    }
     next();
   }
 }
