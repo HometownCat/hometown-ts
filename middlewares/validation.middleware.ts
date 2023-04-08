18b95a1bf89571f97a93ff582824fd519d692231
ybchar <uiurihappy@sju.ac.kr>
2023-04-09 00:16:08 +0900

    feat: middleware validation

--- middlewares/validation.middleware.ts
+++ middlewares/validation.middleware.ts
@@ -2,7 +2,9 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 import { ConfigService } from '@Src/config/config.service';
 import { Request, Response, NextFunction } from 'express';
 import * as response from '../src/common/tools/response.tool';
+import * as dotenv from 'dotenv';
 
+dotenv.config();
 @Injectable()
 export class ValidationMiddleware implements NestMiddleware {
   constructor(private readonly configService: ConfigService) {}
@@ -10,7 +12,7 @@ export class ValidationMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const { 'x-api-key': apiKey } = req.headers;
 
-    if ('greqgeaqrrg@!!!' === this.configService.get('API_KEY')) {
+    if (apiKey === this.configService.get('API_KEY')) {
       next();
     } else {
       response.invalidKey();
